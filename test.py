05bb1a015ba2d4e4867fa6fff3907e9327385dbd
Bally98 <ballyhojayev@gmail.com>
2023-08-07 20:32:05 +0500

    321

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ from datetime import datetime, timedelta
 import datetime as DT
 from functions import create_text, push_post
 
+
+
+
 url_cmc = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 api_key_cmc = '3c6565ce-18c1-4496-8727-02b12ece3299'
 headers_cmc = {
@@ -21,48 +24,114 @@ params_cmc = {
 }
 data_cmc_cap = requests.get(url_cmc, params=params_cmc, headers=headers_cmc).json()
 data_cmc = requests.get(url_cmc, params=params_cmc, headers=headers_cmc).json()
-top1_name = ''
-top2_name = ''
-top3_name = ''
-top1_volume_24h = 0
-top2_volume_24h = 0
-top3_volume_24h = 0
 
-los1_name = ''
-los2_name = ''
-los3_name = ''
-los1_volume_24h = 0
-los2_volume_24h = 0
-los3_volume_24h = 0
+top_names = ['', '', '']
+top_volumes_24h = [0, 0, 0]
 
 for cmc_value in data_cmc['data']:
     percent_change = cmc_value['quote']['USD']['percent_change_24h']
-    if percent_change > top1_volume_24h:
-        top3_volume_24h = top2_volume_24h
-        top2_volume_24h = top1_volume_24h
-        top1_volume_24h = round(float(cmc_value['quote']['USD']['percent_change_24h']), 2)
-        top1_name = cmc_value['name']
-    elif percent_change > top2_volume_24h:
-        top3_volume_24h = top2_volume_24h
-        top2_volume_24h = round(float(cmc_value['quote']['USD']['percent_change_24h']), 2)
-        top2_name = cmc_value['name']
-    elif percent_change > top3_volume_24h:
-        top3_volume_24h = round(float(cmc_value['quote']['USD']['percent_change_24h']), 2)
-        top3_name = cmc_value['name']
-    elif percent_change < los2_volume_24h:
-        los3_volume_24h = los2_volume_24h
-        los2_volume_24h = los1_volume_24h
-        los1_volume_24h = round(float(cmc_value['quote']['USD']['percent_change_24h']), 2)
-        los1_name = cmc_value['name']
-    elif percent_change < los2_volume_24h:
-        los3_volume_24h = los2_volume_24h
-        los2_volume_24h = round(float(cmc_value['quote']['USD']['percent_change_24h']), 2)
-        los2_name = cmc_value['quote']['USD']['percent_change_24h']
-    elif percent_change < los3_volume_24h:
-        los3_volume_24h = round(float(cmc_value['quote']['USD']['percent_change_24h']), 2)
-        los3_name = cmc_value['name']
-print(f'{top1_name}: {top1_volume_24h}\n{top2_name}: {top2_volume_24h}\n{top3_name}: {top3_volume_24h}')
-print(f'{los1_name}: {los1_volume_24h}\n{los2_name}: {los2_volume_24h}\n{los3_name}: {los3_volume_24h}')
+    if percent_change > top_volumes_24h[0]:
+        top_volumes_24h[2] = top_volumes_24h[1]
+        top_volumes_24h[1] = top_volumes_24h[0]
+        top_volumes_24h.insert(0, round(float(cmc_value['quote']['USD']['percent_change_24h']), 2))
+        top_names.insert(0, cmc_value['name'])
+    elif percent_change > top_volumes_24h[1]:
+        top_volumes_24h[2] = top_volumes_24h[1]
+        top_volumes_24h.insert(1, round(float(cmc_value['quote']['USD']['percent_change_24h']), 2))
+        top_names.insert(1, cmc_value['name'])
+    elif percent_change > top_volumes_24h[2]:
+        top_volumes_24h.insert(2, round(float(cmc_value['quote']['USD']['percent_change_24h']), 2))
+        top_names.insert(2, cmc_value['name'])
+# print(f'{top_names[0]}: {top_volumes_24h[0]}\n{top_names[1]}: {top_volumes_24h[1]}\n{top_names[2]}: {top_volumes_24h[2]}')
 
+top3 = ''
 
+for i in range(3):
+    top3 += f"<li>{top_names[i]} - {top_volumes_24h[i]}%</li>"
 
+print(top3)
+
+binance_top2_gain_name = ''
+binance_top2_gain_price_change = 0
+binance_top2_gain_price = 0
+binance_top2_gain_vol = 0
+
+binance_top2_los_name = ''
+binance_top2_los_price_change = 0
+binance_top2_los_price = 0
+binance_top2_los_vol = 999999999
+
+binance_top3_gain_name = ''
+binance_top3_gain_price_change = 0
+binance_top3_gain_price = 0
+binance_top3_gain_vol = 0
+
+binance_top3_los_name = ''
+binance_top3_los_price_change = 0
+binance_top3_los_price = 0
+binance_top3_los_vol = 999999999
+
+gain_los_names = []
+gain_los_price = []
+gain_los_change = []
+gain_los_vol = []
+
+cb_top1_gain_name = ''
+cb_top1_gain_price_change = 0
+cb_top1_gain_price = 0
+cb_top1_gain_vol = 0
+
+cb_top1_los_name = ''
+cb_top1_los_price_change = 0
+cb_top1_los_price = 0
+cb_top1_los_vol = 999999999
+
+cb_top2_gain_name = ''
+cb_top2_gain_price_change = 0
+cb_top2_gain_price = 0
+cb_top2_gain_vol = 0
+
+cb_top2_los_name = ''
+cb_top2_los_price_change = 0
+cb_top2_los_price = 0
+cb_top2_los_vol = 999999999
+
+cb_top3_gain_name = ''
+cb_top3_gain_price_change = 0
+cb_top3_gain_price = 0
+cb_top3_gain_vol = 0
+
+cb_top3_los_name = ''
+cb_top3_los_price_change = 0
+cb_top3_los_price = 0
+cb_top3_los_vol = 999999999
+
+okex_top1_gain_name = ''
+okex_top1_gain_price = 0
+okex_top1_gain_price_change = 0
+okex_top1_gain_vol = 0
+
+okex_top1_los_name = ''
+okex_top1_los_price = 0
+okex_top1_los_price_change = 0
+okex_top1_los_vol = 999999999
+
+okex_top2_gain_name = ''
+okex_top2_gain_price = 0
+okex_top2_gain_price_change = 0
+okex_top2_gain_vol = 0
+
+okex_top2_los_name = ''
+okex_top2_los_price = 0
+okex_top2_los_price_change = 0
+okex_top2_los_vol = 999999999
+
+okex_top3_gain_name = ''
+okex_top3_gain_price = 0
+okex_top3_gain_price_change = 0
+okex_top3_gain_vol = 0
+
+okex_top3_los_name = ''
+okex_top3_los_price = 0
+okex_top3_los_price_change = 0
+okex_top3_los_vol = 999999999
